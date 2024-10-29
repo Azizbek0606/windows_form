62b5f72e4c1099aa5afb98117936b4d69d8126de
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-10-29 10:48:34 +0500

    update

---
+++ WPF/data binding/data binding/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace data_binding
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+        private string boundText;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set {
+                boundText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BoundText"));
+            }
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Surprayz madi fadi";
+        }
+    }
+}
